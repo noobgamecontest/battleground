16ab4e3a4565c3d595430056b447c3eec74205f3
William <work@suppo.fr>
2019-08-14 23:22:55 +0200

    Un 1er test pour la route.

---
+++ tests/Unit/TournamentServiceTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Team;
+use App\Models\Tournament;
+use App\Services\Tournament\TournamentService;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class TournamentServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_init_a_tournament_with_4_slots_and_2_teams_by_match_with_1_winner()
+    {
+        $tournament = factory(Tournament::class)->create([
+            'slots' => 4,
+            'opponents_by_match' => 2,
+            'winners_by_match' => 1,
+        ]);
+
+        $tournament->teams()->saveMany(factory(Team::class, 4)->make());
+
+        $service = new TournamentService();
+
+        $service->init($tournament);
+
+        $this->assertEquals(3, $tournament->matches->count());
+        $this->assertEquals(1, $tournament->matches->max('round'));
+        $this->assertEquals(2, $tournament->matches->get(0)->teams->count());
+        $this->assertEquals(2, $tournament->matches->get(1)->teams->count());
+        $this->assertEquals(0, $tournament->matches->get(2)->teams->count());
+    }
+}
